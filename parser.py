d0fd56dedfcd1f7455bd67263fe63d6406fa72f6
Michael Simpson <michael.simpson@tufts.edu>
2020-03-05 11:25:06 -0500

    add parsing code BUG: atom objects are sometimes out of order'

--- parser.py
+++ parser.py
@@ -5,4 +5,26 @@ import sys
 if len(sys.argv) < 2:
     raise Exception('needs a filename')
 
+kegg_name = 'NAME_NOT_FOUND'
+
+num_atoms = 0
+atom_list = []
+
 filename = sys.argv[1]
+with open(filename, 'r') as file:
+    # get KEGG name
+    tokens = file.readline().split()
+    kegg_name = tokens[1]
+
+    # get number of atoms
+    tokens = file.readline().split()
+    num_atoms = int(tokens[1])
+
+    # read atoms into array
+    for i in range(num_atoms):
+        tokens = file.readline().split()
+        atom = {tokens[1], (tokens[3], tokens[4])}
+        atom_list.append(atom)
+
+for atom in atom_list:
+    print(atom)
